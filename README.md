e1b9ffb35bcf87317d8418663a8b8bd8e0afdbe8
Vasyl Moskalov <vasyl@h2i.sg>
2019-02-14 16:54:37 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# cucumber-junit-formatter
+Cucmber.js junit xml formatter implementing event protocol. In fact this is fast and dirty hack. But I hope I do some work to improve and cleanup code. 
+
+Based on [cucmber-pretty](https://github.com/kozhevnikov/cucumber-pretty) and [cucmber-junit](https://github.com/stjohnjohnson/cucumber-junit).
+'pending' or 'undefined' steps will be reported in xml as failures.
+
+## Installation
+```
+npm install --save-dev https://github.com/mvtm-dn/cucumber-junit-formatter.git
+```
+
+## Use
+```
+cucumber-js -f node_modules/cucumber-junit-formatter:<output-file>
+```
+
+
+## References
+
+- https://github.com/cucumber/cucumber-js/blob/master/docs/cli.md#formats
+- https://github.com/cucumber/cucumber-js/blob/master/docs/custom_formatters.md
+- https://stackoverflow.com/questions/4922867/what-is-the-junit-xml-format-specification-that-hudson-supports
+- http://help.catchsoftware.com/display/ET/JUnit+Format
+- https://www.ibm.com/support/knowledgecenter/en/SSUFAU_1.0.0/com.ibm.rsar.analysis.codereview.cobol.doc/topics/cac_useresults_junit.html
---
+++ index.js
@@ -0,0 +1,119 @@
+const { Formatter, formatterHelpers } = require('cucumber');
+const xml = require('xml');
+
+// Next two methods taken from https://github.com/stjohnjohnson/cucumber-junit/blob/master/lib/cucumber_junit.js
+let createProperty=(name, value)=>{
+        return {
+            property: [
+                {
+                    _attr: {
+                        name: name,
+                        value: value
+                    }
+                }
+            ]
+        };
+    },
+
+    createFailure=(message)=>{
+        return {
+            failure: [
+                { _attr: { message: message.split("\n").shift() } },
+                message
+            ]
+        };
+    },
+    
+    createFailureOrError=(type,exception)=>{
+        let {name}=exception,
+        ret ={};
+        ret[type]=[
+                { _attr: { message: name } },
+                formatterHelpers.formatError(exception)
+            ];
+        return ret;
+    },
+    
+    // Next one from original json_formatter
+    convertNameToId=(obj)=>{
+        return obj.replace(/ /g, '-').toLowerCase();
+    };
+    
+
+
+class JUnitFormatter extends Formatter {
+  /** @param {Options} options */
+  constructor(options) {
+    super(options);
+
+    options.eventBroadcaster.on('test-run-started', ()=>{
+        this._result=[];
+    });
+
+    options.eventBroadcaster.on('test-case-finished', ({ sourceLocation })=>{    
+      const { gherkinDocument, pickle, testCase } = options.eventDataCollector.getTestCaseData(sourceLocation);
+      let testSuiteId=`${convertNameToId(gherkinDocument.feature.name)}';'${convertNameToId(pickle.name)}`;
+      let attr={name:testSuiteId,tests:0,failures:0,skipped:0,"package":convertNameToId(pickle.name),errors:0,time:(testCase.result.duration/1000).toFixed(3)},
+          testSuite=[{_attr:attr}];
+          
+      if (pickle.tags.length) {
+        testSuite.push({properites:pickle.tags.map(tag=>createProperty("tag",tag.name))});
+      }
+      
+      testCase.steps.forEach((step,index)=>{
+        const {gherkinKeyword, pickleStep } = options.eventDataCollector.getTestStepData({testCase:testCase,index:index});
+        if (gherkinKeyword || step.result==='failed') {
+            let testCaseId=convertNameToId(gherkinKeyword?pickleStep.text:(index?'after':'before'));
+            let testCase=[
+                {
+                    _attr:{
+                        classname:testCaseId,
+                        name:gherkinKeyword?pickleStep.text:(pickle.name+(index?' after':' before')),
+                        time:((step.result.duration | 0)/1000).toFixed(3)
+                    }
+                }
+            ];
+            switch (step.result.status) {
+                case 'passed':
+                    break;
+                case 'failed':
+                    testCase.push(createFailureOrError(gherkinKeyword?"failure":"error",step.result.exception));
+                    if (gherkinKeyword) {
+                        attr.failures+=1;
+                    }
+                    else {
+                        attr.errors+=1;
+                    }
+                    break;
+                case 'pending':
+                case 'undefined':
+                    testCase.push(createFailure(step.result.status === 'pending' ? 'Pending' 
+                            : `Undefined step. Implement with the following snippet:\n  ${gherkinKeyword.trim()}(/^${pickleStep.text}$/, function(callback) {\n      // Write code here that turns the phrase above into concrete actions\n      callback(null, 'pending');\n  });`
+                    ));
+                    attr.failures+=1;
+                    break;
+                case 'skipped':
+                    testCase.push({skipped: []});
+                    attr.skipped+=1;
+                    break;
+                default:
+                    testCase.push(createFailure(`Unknown status - ${step.result.status}`));
+            }
+            attr.tests+=1;
+            testSuite.push({testcase:testCase});
+        }
+      });
+      this._result.push({testsuite:testSuite});
+    
+    });
+
+
+    options.eventBroadcaster.on('test-run-finished', ()=>{
+        this.log(xml({ testsuites: this._result }, {indent:'  ',declaration: { encoding: 'UTF-8' }}));
+    });
+  }
+  
+}
+
+module.exports = JUnitFormatter;
+
